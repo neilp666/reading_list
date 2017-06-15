f44e54c8d4c220fcc4d863debf5c2607983d8540
neil patel <neil@myindianweddingapp.com>
2017-06-15 12:56:36 +0100

    Created books scaffold & seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.destroy_all
+
+Book.create!([{
+  title: "The Path: A New Way to Think About Everything",
+  author: "Professor Michael Puett",
+  description: "Covering subjects from decision-making to relationships, The Path applies ancient Chinese philosophies to demonstrate how to become happier and more productive through making simple changes to our 
+  everyday routines. A profound guide to living well",
+  amazon_id: "0241004497"
+  },
+
+  {
+  title: "The Future of Almost Everything: The global changes that will affect every business and all our lives",
+  author: "Patrick Dixon",
+  description: "From the man the Wall Street Journal describes as a 'global change guru', more than one hundred of the trends that touch every aspect of our lives.
+Patrick Dixon looks at how the future will be Fast, Urban, Tribal, Universal, Radical and Ethical - a future of boom and bust and great economic change as the emerging markets grow up",
+  amazon_id: "1781254974"
+  }])
+
+p "Created #{Book.count} books"
+
