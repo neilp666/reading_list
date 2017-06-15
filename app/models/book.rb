defb38de0ee2bf6ab2935bbbbca553fa59aa7817
neil patel <neil@myindianweddingapp.com>
2017-06-15 17:17:37 +0100

    Setting up search feature

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ApplicationRecord
 
   scope :finished, -> { where.not(finished_on: nil) }
   scope :recent, -> { where('finished_on > ?', 2.days.ago) }
+  scope :search, -> (keyword) { where(title: keyword) if keyword.present? }
+
+
 
   def finished?
     finished_on.present?
